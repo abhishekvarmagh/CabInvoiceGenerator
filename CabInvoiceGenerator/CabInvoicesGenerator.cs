3c8c56d5bde11258c2d09c64bba1f6e288b7052a
abhishekvarmagh <abhishektheverma@gmail.com>
2020-08-17 19:11:13 +0530

    [ADDED]Abhishek : TestCase And Code To Return Total Fare For Given Distance And Time

---
+++ CabInvoiceGenerator/CabInvoicesGenerator.cs
@@ -0,0 +1,31 @@
+﻿// <copyright file="CabInvoicesGenerator.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace CabInvoiceGenerator
+{
+    using System;
+
+    /// <summary>
+    /// Cab Invoice Generator Class.
+    /// </summary>
+    public class CabInvoicesGenerator
+    {
+        private static readonly double CostPerKilometer = 10.0;
+        private static readonly double CostPerMinute = 1.0;
+        private static readonly double MinimumFare = 5.0;
+        private double totalFare = 0.0;
+
+        /// <summary>
+        /// Function To Calculate Fare.
+        /// </summary>
+        /// <param name="distance">Distance.</param>
+        /// <param name="time">Time.</param>
+        /// <returns>Total Fare.</returns>
+        public double CalculateFare(double distance, int time)
+        {
+            this.totalFare = (distance * CostPerKilometer) + (time * CostPerMinute);
+            return Math.Max(this.totalFare, MinimumFare);
+        }
+    }
+}
--- CabInvoiceGeneratorTest/CabInvoiceTest.cs
+++ CabInvoiceGeneratorTest/CabInvoiceTest.cs
@@ -1,18 +1,50 @@
-using NUnit.Framework;
+// <copyright file="CabInvoiceTest.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace CabInvoiceGeneratorTest
 {
+    using CabInvoiceGenerator;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Cab Invoice Test Class.
+    /// </summary>
     public class CabInvoiceTest
     {
+        private CabInvoicesGenerator cabInvoiceGenerator;
+
+        /// <summary>
+        /// Provide a common set of functions that are performed just before each test method is called.
+        /// </summary>
         [SetUp]
         public void Setup()
         {
+            this.cabInvoiceGenerator = new CabInvoicesGenerator();
         }
 
+        /// <summary>
+        /// Given Distance And Time When Greater Than Minimum Fare Should Return Total Fare.
+        /// </summary>
         [Test]
-        public void Test1()
+        public void GivenDistanceAndTime_ShouldReturnTotalFare()
         {
-            Assert.Pass();
+            double distance = 5.0;
+            int time = 5;
+            double totalFare = this.cabInvoiceGenerator.CalculateFare(distance, time);
+            Assert.AreEqual(55.0, totalFare);
+        }
+
+        /// <summary>
+        /// Given Distance And Time When Less Than Minimum Fare Should Return Minimum Fare.
+        /// </summary>
+        [Test]
+        public void GivenDistanceAndTime_ShouldReturnMinimumTotalFare()
+        {
+            double distance = 0.1;
+            int time = 1;
+            double totalFare = this.cabInvoiceGenerator.CalculateFare(distance, time);
+            Assert.AreEqual(5.0, totalFare);
         }
     }
 }
